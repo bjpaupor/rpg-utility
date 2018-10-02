9dbbfb551977db75997044c3dbe4c5f56d521c55
brandon <bjpaupor@mtu.edu>
2018-10-02 18:37:13 -0400

    Added support for pathfinder classes

--- Java/src/pathfinder/BABProgression.java
+++ Java/src/pathfinder/BABProgression.java
@@ -1,5 +1,9 @@
 package pathfinder;
-
+/**
+ * How quickly base attack bonus increases
+ * @author brandon
+ *
+ */
 public enum BABProgression {
 	FAST, MEDIUM, SLOW;
 }
--- Java/src/pathfinder/Class.java
+++ Java/src/pathfinder/Class.java
@@ -1,5 +1,8 @@
 package pathfinder;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+
 public class Class {
 	private String name;
 	private String description; //description role
@@ -22,6 +25,44 @@ public class Class {
 		this.features = features;
 		this.bab = bab;
 		this.goodSaves = goodSaves;
+	}
+	//Filename doesn't follow convention because .class files are already used
+	public Class(String fileName) {
+		if (!fileName.endsWith(".pclass")) {
+			System.out.println("-> " + fileName + ": Incorrect file type, file must be .pclass\n");
+			return;
+		}
+		else 
+			try (BufferedReader read = new BufferedReader(new FileReader(fileName))){
+				name = Tools.readALine(read);
+				description = Tools.readALine(read);
+				alignments = new Alignment[Integer.parseInt(Tools.readALine(read))];
+				if (alignments.length == 9)
+					alignments = Alignment.values();
+				else
+					for (int i = 0; i < alignments.length; i++)
+						alignments[i] = Alignment.valueOf(Tools.readALine(read).toUpperCase());
+				hitDie = HitDice.valueOf(Tools.readALine(read).toLowerCase());
+				skills = new String[Integer.parseInt(Tools.readALine(read))];
+				for (int i = 0; i < skills.length; i++)
+					skills[i] = Tools.readALine(read);
+				skillRanksPerLevel = Integer.parseInt(Tools.readALine(read));
+				features = new Feature[Integer.parseInt(Tools.readALine(read))];
+				for (int i = 0; i < features.length; i++)
+					features[i] = new Feature(Tools.readALine(read), Tools.readALine(read));
+				bab = BABProgression.valueOf(Tools.readALine(read).toUpperCase());
+				goodSaves = new Save[Integer.parseInt(Tools.readALine(read))];
+				if (goodSaves.length == 3)
+					goodSaves = Save.values();
+				else
+					for (int i = 0; i < goodSaves.length; i++)
+						goodSaves[i] = Save.valueOf(Tools.readALine(read).toUpperCase());
+			}
+			catch (Exception ex) {
+				System.out.println("->" + getName() + ": Failed to interpret class file!");
+				ex.printStackTrace();
+				return;
+			}	
 	}
 	public String getDescription() {
 		return description;
