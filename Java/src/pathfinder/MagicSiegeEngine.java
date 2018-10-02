7892b1eb52731db73359df511e483ca22ee006df
brandon <bjpaupor@mtu.edu>
2018-10-02 13:35:35 -0400

    Added standardized activation to magical siege engines

--- Java/src/pathfinder/MagicSiegeEngine.java
+++ Java/src/pathfinder/MagicSiegeEngine.java
@@ -6,7 +6,7 @@ public class MagicSiegeEngine extends MagicWeapon {
 
 	public MagicSiegeEngine(String name, String description, String price, String weight, String material, int hp,
 			int breakDC, String damage, String critical, int range, DamageType damageType, Feature[] features,
-			Proficiency proficiency, Size size, int enhancement, SpecialAbility[] specialAbilities, String activation,
+			Proficiency proficiency, Size size, int enhancement, SpecialAbility[] specialAbilities, Activation activation,
 			String[] requirements, String cost, int crew, Armor armor) {
 		super(name, description, price, weight, material, hp, breakDC, damage, critical, range, damageType, features,
 				proficiency, size, enhancement, specialAbilities, activation, requirements, cost);
