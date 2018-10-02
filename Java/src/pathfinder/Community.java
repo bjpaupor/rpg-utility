9a75daebfc79eff0b382a0161785b74f29bdc5c4
brandon <bjpaupor@mtu.edu>
2018-10-02 19:49:58 -0400

    Added functionality for Communities

--- Java/src/pathfinder/Community.java
+++ Java/src/pathfinder/Community.java
@@ -1,5 +1,12 @@
 package pathfinder;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+/**
+ * A gathering of civilization
+ * @author brandon
+ *
+ */
 public class Community {
 	public enum Type {
 		THORP, HAMLET, VILLAGE, SMALL_TOWN, LARGE_TOWN, SMALL_CITY, LARGE_CITY, METROPOLIS;
@@ -7,12 +14,12 @@ public class Community {
 	private String name;
 	private String description; //Laws, Walls, Guard, Paths, Locations, Lighting
 	private Type type;
-	private double baseValue;
+	private String baseValue; //in gp
 	private String minorItems;
 	private String mediumItems;
 	private String majorItems;
 	
-	public Community(String name, String description, Type type, double baseValue, String minorItems,
+	public Community(String name, String description, Type type, String baseValue, String minorItems,
 			String mediumItems, String majorItems) {
 		this.name = name;
 		this.description = description;
@@ -21,6 +28,27 @@ public class Community {
 		this.minorItems = minorItems;
 		this.mediumItems = mediumItems;
 		this.majorItems = majorItems;
+	}
+	public Community(String fileName) {
+		if (!fileName.endsWith(".community")) {
+			System.out.println("-> " + fileName + ": Incorrect file type, file must be .community\n");
+			return;
+		}
+		else 
+			try (BufferedReader read = new BufferedReader(new FileReader(fileName))){
+				name = Tools.readALine(read);
+				description = Tools.readALine(read);
+				type = Type.valueOf(Tools.readALine(read).toUpperCase());
+				baseValue = Tools.readALine(read);
+				minorItems = Tools.readALine(read);
+				mediumItems = Tools.readALine(read);
+				majorItems = Tools.readALine(read);
+			}
+			catch (Exception ex) {
+				System.out.println("->" + getName() + ": Failed to interpret community file!");
+				ex.printStackTrace();
+				return;
+			}	
 	}
 	public String getName() {
 		return name;
@@ -31,7 +59,7 @@ public class Community {
 	public Type getType() {
 		return type;
 	}
-	public double getBaseValue() {
+	public String getBaseValue() {
 		return baseValue;
 	}
 	public String getMinorItems() {
