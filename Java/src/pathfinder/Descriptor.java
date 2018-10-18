9d4eb4846dccffb6b5849ea633659eb2a5600af5
brandon <bjpaupor@mtu.edu>
2018-10-18 01:10:39 -0400

    Added support for feats

--- Java/src/pathfinder/Descriptor.java
+++ Java/src/pathfinder/Descriptor.java
@@ -1,5 +1,8 @@
 package pathfinder;
-
+/**
+ * Describing an effect
+ * @author brandon
+ */
 public enum Descriptor {
 	ACID, AIR, CHAOTIC, COLD, DARKNESS, DEATH, EARTH, ELECTRICITY, EVIL, FEAR, FIRE, FORCE, GOOD, LANGUAGE_DEPENDENT,
 	LAWFUL, LIGHT, MIND_AFFECTING, SONIC, WATER;
--- Java/src/pathfinder/Feat.java
+++ Java/src/pathfinder/Feat.java
@@ -1,12 +1,38 @@
 package pathfinder;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+/**
+ * Special ability with a type
+ * @author brandon
+ */
 public class Feat {
+	/**
+	 * The type of feat
+	 * @author brandon
+	 */
 	public class Type {
 		private String name;
 		private String description;
 		public Type(String name, String description) {
 			this.name = name;
 			this.description = description;
+		}
+		public Type(String fileName) {
+			if (!fileName.endsWith(".featType")) {
+				System.out.println("-> " + fileName + ": Incorrect file type, file must be .featType\n");
+				return;
+			}
+			else 
+				try (BufferedReader read = new BufferedReader(new FileReader(fileName))){
+					name = Tools.readALine(read);
+					description = Tools.readALine(read);
+				}
+				catch (Exception ex) {
+					System.out.println("->" + getName() + ": Failed to interpret feat type file!");
+					ex.printStackTrace();
+					return;
+				}	
 		}
 		public String getName() {
 			return name;
@@ -24,6 +50,23 @@ public class Feat {
 		this.name = name;
 		this.description = description;
 		this.type = type;
+	}
+	public Feat(String fileName) {
+		if (!fileName.endsWith(".feat")) {
+			System.out.println("-> " + fileName + ": Incorrect file type, file must be .feat\n");
+			return;
+		}
+		else 
+			try (BufferedReader read = new BufferedReader(new FileReader(fileName))){
+				name = Tools.readALine(read);
+				description = Tools.readALine(read);
+				type = Tools.readALine(read);
+			}
+			catch (Exception ex) {
+				System.out.println("->" + getName() + ": Failed to interpret feat file!");
+				ex.printStackTrace();
+				return;
+			}	
 	}
 	public String getName() {
 		return name;
