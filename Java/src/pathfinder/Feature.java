11459afea3fcbeca73a44625a8bedbf1676f1b7e
brandon <bjpaupor@mtu.edu>
2018-10-18 16:51:17 -0400

    Added support for features.

--- Java/src/pathfinder/Feature.java
+++ Java/src/pathfinder/Feature.java
@@ -1,5 +1,11 @@
 package pathfinder;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+/**
+ * Special ability
+ * @author brandon
+ */
 public class Feature {
 	private String name;
 	private String description;
@@ -8,7 +14,22 @@ public class Feature {
 		this.name = name;
 		this.description = description;
 	}
-	
+	public Feature(String fileName) {
+		if (!fileName.endsWith(".pfeature")) {
+			System.out.println("-> " + fileName + ": Incorrect file type, file must be .pfeature\n");
+			return;
+		}
+		else 
+			try (BufferedReader read = new BufferedReader(new FileReader(fileName))){
+				name = Tools.readALine(read);
+				description = Tools.readALine(read);
+			}
+			catch (Exception ex) {
+				System.out.println("->" + getName() + ": Failed to interpret feature file!");
+				ex.printStackTrace();
+				return;
+			}	
+	}
 	public String getName() {
 		return name;
 	}
--- Java/src/pathfinder/HitDice.java
+++ Java/src/pathfinder/HitDice.java
@@ -1,5 +1,8 @@
 package pathfinder;
-
+/**
+ * The die to roll per level in a class/race
+ * @author brandon
+ */
 public enum HitDice {
 	d6, d8, d10, d12;
 }
